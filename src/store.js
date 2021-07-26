1c644ea05d9abdc4a2f4de5aaff22b41fa2d6d1c
Erick Contreras <eri89--@hotmail.com>
2021-07-26 17:41:50 -0500

    Implement arrow function on store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-export function savingOnLocal(arr) {
-  localStorage.setItem('tasks', JSON.stringify(arr));
-}
+let savingOnLocal = (arr) => localStorage.setItem('tasks', JSON.stringify(arr));
 
-export const retrieveLocal = () => JSON.parse(localStorage.getItem('tasks'));
+const retrieveLocal = () => JSON.parse(localStorage.getItem('tasks'));
+
+export { savingOnLocal, retrieveLocal };
