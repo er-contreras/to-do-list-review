f6b499412c87c1bbb1ccfccad087d78a9a5ac70b
Erick Contreras <eri89--@hotmail.com>
2021-07-26 17:45:44 -0500

    Fix linters on store.js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-let savingOnLocal = (arr) => localStorage.setItem('tasks', JSON.stringify(arr));
+const savingOnLocal = (arr) => localStorage.setItem('tasks', JSON.stringify(arr));
 
 const retrieveLocal = () => JSON.parse(localStorage.getItem('tasks'));
 
